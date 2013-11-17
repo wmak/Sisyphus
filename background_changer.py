449916f23daf0c27a0fea31115959c0ae08fa2a4
wmak <william.mak@hotmail.ca>
2013-11-16 22:08:58 -0500

    adding configuration capability

--- background_changer.py
+++ background_changer.py
@@ -1,8 +1,15 @@
 import subprocess
 import time
+import ConfigParser
+import os
+
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+path = config.get("Settings", "image_folder")
 
 while 1:
 	hour = time.localtime().tm_hour
-	background="file:////home/wmak/Pictures/backgrounds/" + str(hour) + ".jpg"
+	picture = os.path.join(path, str(hour) + ".jpg")
+	background="file:///" + picture
 	subprocess.call(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', background])
 	time.sleep(300)
