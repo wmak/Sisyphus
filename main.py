a40443e52c64de239c70406e5150cca81735b203
wmak <william.mak@hotmail.ca>
2013-11-16 22:24:36 -0500

    Renaming background_changer to main. Refactoring code a bit as well

---
+++ main.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+import datetime
+import ConfigParser
+import os
+
+def parse_config():
+	config = ConfigParser.ConfigParser()
+	config.read("config.ini")
+	return config
+
+def change_background():
+	hour = time.localtime().tm_hour # get current hour
+	picture = os.path.join(path, str(hour) + ".jpg") # get the corresponding picture
+	background="file:///" + picture
+	subprocess.call(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', background]) #Change the picture
+
+config = parse_config()
+path = config.get("Settings", "image_folder")
+
+# main loop
+while 1:
+	change_background()
+	wait = 60 - datetime.datetime.today().time().minute #Calculate how long to wait
+	time.sleep(wait*60)
